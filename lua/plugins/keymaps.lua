eab99e54e96d1ca829abbe1418482604ee6a2208
暴躁老爸 <dad@baozaolaoba.top>
2025-04-21 10:16:08 +0800

    feat(keymap): c-s-v 支持粘贴

--- lua/plugins/keymaps.lua
+++ lua/plugins/keymaps.lua
@@ -1,2 +1,20 @@
 vim.keymap.set("x", ">", ">gv", { desc = "Visual indent right and reselect" })
 vim.keymap.set("x", "<", "<gv", { desc = "Visual indent left and reselect" })
+
+
+-- 粘贴：插入模式下 Ctrl+Shift+V
+vim.keymap.set("i", "<C-S-v>", "<C-r>+", { desc = "Paste clipboard in insert mode" })
+
+-- 粘贴：普通模式下 Ctrl+Shift+V
+vim.keymap.set("n", "<C-S-v>", '"+p', { desc = "Paste clipboard in normal mode" })
+
+-- 粘贴：视觉模式下 Ctrl+Shift+V（替换选中的文字）
+vim.keymap.set("v", "<C-S-v>", '"+p', { desc = "Paste clipboard in visual mode" })
+
+-- command mode
+vim.keymap.set("c", "<C-S-v>", "<C-r>+", { desc = "Paste clipboard in command mode" })
+
+-- 绑定 Ctrl+Shift+V 为粘贴功能
+local opts = { noremap = true, silent = true }
+vim.api.nvim_set_keymap("n", "<C-S-V>", '"+p', opts) -- 普通模式粘贴
+vim.api.nvim_set_keymap("v", "<C-S-V>", '"+p', opts) -- 可视模式粘贴
